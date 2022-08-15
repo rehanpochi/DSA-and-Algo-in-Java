abfe223f2cf5549546bfce14a6ca27849439dd89
rehan <rehanpochi1@gmail.com>
2022-08-16 02:37:23 +0530

    Finding no of steps to reduce a number to zero.
    Signed-off-by: rehan <rehanpochi1@gmail.com>

---
+++ ReduceToZero.java
@@ -0,0 +1,18 @@
+public class ReduceToZero {
+    public static void main(String[] args) {
+        System.out.println(reduce(14));
+    }
+
+    static int reduce(int n) {
+        int count = 0;
+        if (n == 0) {
+            return 0;
+        }
+        count++;
+        if (n % 2 == 0) {
+            return count + reduce(n / 2);
+        } else {
+            return count + reduce(n - 1);
+        }
+    }
+}
